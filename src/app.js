73c0838a35829d0796f2a03ca3dd7fed7b642d9c
TuanySantosFesta@2019git statusgit config --global user.name TuanySantos <tuanybsantos@gmail.com>
2020-09-14 16:43:49 -0300

    configurando a porta do heroku

--- src/app.js
+++ src/app.js
@@ -11,4 +11,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 require("./controllers/authController")(app);
 require("./controllers/projectController")(app);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
